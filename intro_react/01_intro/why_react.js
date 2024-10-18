867f3ac0c86e8513c7afb7c2d76314d86de03f9e
surajllpl <srj.llpl@gmail.com>
2024-10-18 15:32:45 +0530

    why react

---
+++ intro_react/01_intro/why_react.js
@@ -0,0 +1,37 @@
+// why React
+// React is a JavaScript library for building user interfaces. It's used for creating reusable UI components.
+// React allows you to create small, reusable pieces of code called "components" that can be easily
+// the concept of virtual DOM
+// Virtual DOM is a lightweight in-memory representation of the real DOM.
+// When the state of the application changes, React updates the virtual DOM, and then efficiently updates the
+// real DOM by comparing the two and only making the necessary changes.
+
+// Document Object Module (DOM)
+
+//create virtual DOM and changes what update are required.
+
+// you can build web and mobile application.
+
+// React is a Library and Anguler is Framework.
+
+//Framework vs Library
+
+// Framework is a full-featured solution that provides a lot of functionality out of the box.
+
+// Library is a collection of reusable code that you can use to build your application.
+
+// React is a library, it doesn't provide a lot of functionality out of the box, but
+// it provides a lot of flexibility and customization options.
+
+// Library is light-weight flexiblity
+
+
+//React - it feels like a framework
+
+
+
+
+
+
+
+
