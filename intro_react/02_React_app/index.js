cb67f89d4e7bad89a829cc3e9a700ff78dd863ff
surajllpl <srj.llpl@gmail.com>
2024-10-19 11:21:46 +0530

    use css and babel

--- intro_react/02_React_app/index.js
+++ intro_react/02_React_app/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+// console.log("Hello World");
+const rootElement = document.getElementById("root");
+// console.log(rootElement); // it select root element (<div id="root"></div>)
+
+    //type,props,children
+    // function hello(){
+    //     return  React.createElement("h1",{className :"heading"},"Hello World");
+    // }
+    // const element = React.createElement("div", { id: "root" }, "Hello World");
+// const element = React.createElement("h1",{className :"heading"},"Hello World");
+    // console.log(element); // it will return a virtual DOM element
+    function Hello(){
+        return <h1>Hello word</h1>
+    }
+
+ReactDOM.createRoot(rootElement).render(<Hello/>);// use first letter should be Capital of function Hello oterwise it gives error - for understand to JSX
+
+//JSX
+//  multiple changes and call React / ReactDOM it is not possible to handle 
+// so we can used JSX
+// babel can convert  <h1>Hello word</h1> to React.createElement("h1",{className :"heading"},"Hello World");
+//using babel cdn
+
+
+
+
+
