2a6a2d5609277e7a9c04d23c9c0b07f17e2128e2
surajllpl <srj.llpl@gmail.com>
2024-11-07 13:27:09 +0530

    CRA - create component HelloWorld and render it.

---
+++ intro_react/03_Create_React_app_CRA/my-app/src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+function HelloWorld() {
+    return (
+      <> 
+        <h1>Heading</h1>
+        <p>This is para</p>
+      </>
+    );
+  }
+  export default HelloWorld;
--- intro_react/03_Create_React_app_CRA/my-app/src/index.js
+++ intro_react/03_Create_React_app_CRA/my-app/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react'; // it available in nod-module
-import ReactDOM from 'react-dom/client';
+import React from "react"; // it available in nod-module
+import ReactDOM from "react-dom/client";
+import HelloWorld from "./HelloWorld";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Hello() {
+  return (
+    <> 
+      <h1>Hello World !</h1>
+      <p>This is para</p>
+    </>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode> 
-   <h1>Hello World</h1>
+  <React.StrictMode>
+    <HelloWorld />
   </React.StrictMode>
 );
-
