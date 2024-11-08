978fc74c8038912b8f55451eb19c5be72480b669
surajllpl <srj.llpl@gmail.com>
2024-11-08 12:14:36 +0530

    CRA - Add Greeting component

--- intro_react/03_Create_React_app_CRA/my-app/src/App.jsx
+++ intro_react/03_Create_React_app_CRA/my-app/src/App.jsx
@@ -1,9 +1,10 @@
 import HelloWorld from "./components/HelloWorld"
-
+import Greeting from "./components/Greeting"
 function App() {
   return (
     <div className="App">
         <HelloWorld />
+        <Greeting />
     </div>
   )
 }
