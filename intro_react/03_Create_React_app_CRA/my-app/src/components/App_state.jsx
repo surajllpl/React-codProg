8d17ae0b06fa66a15d62ec2e90809433d6f055a5
surajllpl <srj.llpl@gmail.com>
2024-11-21 15:50:32 +0530

    React State - useState() intro

--- intro_react/03_Create_React_app_CRA/my-app/src/components/App_state.jsx
+++ intro_react/03_Create_React_app_CRA/my-app/src/components/App_state.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 //hooks
 // useState
 // useEffect
 
+// useState()
+// - useState(defaultvalue) -----> it return array[stateValue,functionToUpdateThatStateValue]
+// - useState call directly components not call outside of component within function 
+
 
 function App_state() {
-    let firstName ="Suraj"; // variable cannont rerender the UI.
-    function handleChangeName(){  // in react we cannot use normal veriable to change the UI.
-        firstName = "Mohan";
-        console.log("clicked",firstName);
+    // let firstName ="Suraj"; // variable cannont rerender the UI.
+    // function handleChangeName(){  // in react we cannot use normal veriable to change the UI.
+    //     firstName = "Mohan";
+    //     console.log("clicked",firstName);
+    // }
+    console.log("component render");
+    // const returnValue = useState("Suraj");
+    // const firstName = returnValue[0];
+    // const updateFirstName = returnValue[1];
+    const [firstName, setFirstName] = useState("Suraj");
+    function handleChangeName(){
+        if(firstName === "Suraj"){
+            setFirstName("Mohan");// we can use setState to change the UI.
+        }else{
+            setFirstName("Suraj");
+        }
     }
   return (
     <div>
         <h1>{firstName}</h1>
+
         <button onClick={handleChangeName}>clicked me</button>
     </div>
   )
+
+  
 }
 
 export default App_state
