30282cc3a65109f094de5b45a67b5dff1f83b792
surajllpl <srj.llpl@gmail.com>
2024-11-21 16:18:06 +0530

    React State - Counter Example

--- intro_react/03_Create_React_app_CRA/my-app/src/components/Counter_State.jsx
+++ intro_react/03_Create_React_app_CRA/my-app/src/components/Counter_State.jsx
@@ -2,12 +2,28 @@ import React, { useState } from 'react'
 
 function Counter_State() {
     const [count, setCount] = useState(0);
+    function handleIncrease(){
+        setCount(count + 1);
+    }
+    function handleDecrease(){
+        setCount(count - 1);
+    }
+    function handleReset(){
+        setCount(0);
+    }
   return (
     <div>
         <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>Increse</button>
+
+        <button onClick={handleIncrease}>Increse</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleDecrease}>Decrease</button>
+
+
+        {/* // using Arrow function  */}
+        {/* <button onClick={() => setCount(count + 1)}>Increse</button>
         <button onClick={() => setCount(0)}>Reset</button>
-        <button onClick={() => setCount(count - 1)}>Decrease</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button> */}
 
     </div>
   )
