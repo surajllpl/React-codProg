dc1b0daafcf5dac371ac92163104fb0d501a6eab
surajllpl <srj.llpl@gmail.com>
2024-12-11 13:36:34 +0530

    State -state Array Example

--- intro_react/03_Create_React_app_CRA/my-app/src/App_state.jsx
+++ intro_react/03_Create_React_app_CRA/my-app/src/App_state.jsx
@@ -4,22 +4,13 @@ import StateArray from './components/state/State_Array'
 // import SimpleStateExample from './components/state/SimpleStateExample';
 
 function App_state() {
-    const [fruits,setFruits] = useState(["Apple","Mango","Banana"]);
-    function handleAddFruit(){
-        console.log("add fruit");
-        setFruits([...fruits,"Grapes"])
-    }
+    
   return (
     <div>
       {/* <SimpleStateExample />  */}
         {/* < CounterState /> */}
         <h1><StateArray /></h1> 
-        <ul>
-            {fruits.map((fruit, index) => (
-                <li key={index}>{fruit}</li>
-                ))}
-        </ul>
-        <button onClick={handleAddFruit}>Add Fruit</button>
+       
     </div>
   )
 }
--- intro_react/03_Create_React_app_CRA/my-app/src/components/state/State_Array.jsx
+++ intro_react/03_Create_React_app_CRA/my-app/src/components/state/State_Array.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function StateArray() {
+  const [fruits,setFruits]=useState(["Apple","Mango","Banana"]);
+  function handleAddFruit(){
+    console.log("add fruit");
+    setFruits([...fruits,"Grapes"]);
+    // setFruits((prevState)=>{
+    //   return [...prevState,"Grapes"];
+    // });
+}
   return (
-    <div>State Array</div>
+    <div>State Array
+      <ul>
+            {fruits.map((fruit, index) => (
+                <li key={index}>{fruit}</li>
+                ))}
+        </ul>
+        <button onClick={handleAddFruit}>Add Fruit</button>
+    </div>
   )
 }
 
