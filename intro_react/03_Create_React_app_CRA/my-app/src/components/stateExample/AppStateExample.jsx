86cf5dab12ed2372b4a04d08b0e4a3435c60751e
surajllpl <srj.llpl@gmail.com>
2024-12-21 11:59:05 +0530

    state example

---
+++ intro_react/03_Create_React_app_CRA/my-app/src/components/stateExample/AppStateExample.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Users from './stateExample/Users'
+
+function AppStateExample() {
+    const [users,setUsers] = useState([
+        {id : 1, firstName : "suraj", lastName : "Jagtap", age : 25},
+        {id : 2, firstName : "Rahul", lastName : "Kale", age : 27},
+        {id : 3, firstName : "Kunal", lastName : "Bhosale", age : 22},
+        {id : 2, firstName : "Rohan", lastName : "Mehata", age : 24},
+    ])
+  return (
+    <div className='App'>
+        <h1>App State Example</h1>
+        <Users users={users}/>
+        </div>
+  )
+}
+
+export default AppStateExample
